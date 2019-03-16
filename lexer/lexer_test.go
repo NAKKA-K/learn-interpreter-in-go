2d0b49e118a107553a789564a6ae7adb349eae18
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-03-16 22:56:10 +0900

    Add read char test of lexer

---
+++ lexer/lexer_test.go
@@ -0,0 +1,42 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/NAKKA-K/learn-interpreter-in-go/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBARAN, "{"},
+		{token.RBARAN, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
