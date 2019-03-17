723ce56bd07479fa5dc50bc609437d324a8eb5e0
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-03-17 23:36:29 +0900

    Add token determined func

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,10 @@
 package lexer
 
+import (
+	"github.com/NAKKA-K/learn-interpreter-in-go/token"
+)
+
+// Lexer is program reader
 type Lexer struct {
 	input        string
 	position     int  // Last read position
@@ -7,8 +12,10 @@ type Lexer struct {
 	ch           byte // Character pointing to
 }
 
+// New lexer generator. It't constructor
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
 }
 
@@ -20,4 +27,38 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition++
+}
+
+// NextToken to determined
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
