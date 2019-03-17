0d190a032f296447fcdaebc9439508d705a2a3c0
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-03-18 01:11:51 +0900

    Add more operator to lexer

--- token/token.go
+++ token/token.go
@@ -18,8 +18,15 @@ const (
 	INT   = "INT"
 
 	// operator
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 
 	// delimiter
 	COMMA     = ","
