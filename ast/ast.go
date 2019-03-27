39348d086a99c5db590b2d699313bf174725cf5e
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-03-27 16:34:39 +0900

    Add call expression parser

--- ast/ast.go
+++ ast/ast.go
@@ -263,3 +263,29 @@ func (fl *FunctionLiteral) String() string {
 	return out.String()
 }
 func (fl *FunctionLiteral) expressionNode() {}
+
+// CallExpression for call function (in identifier)
+type CallExpression struct {
+	Token     token.Token // '('
+	Function  Expression  // Identifier or FunctionLiteral
+	Arguments []Expression
+}
+
+// TokenLiteral return '('
+func (ce *CallExpression) TokenLiteral() string { return ce.Token.Literal }
+func (ce *CallExpression) String() string {
+	var out bytes.Buffer
+
+	args := []string{}
+	for _, a := range ce.Arguments {
+		args = append(args, a.String())
+	}
+
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
+
+	return out.String()
+}
+func (ce *CallExpression) expressionNode() {}
