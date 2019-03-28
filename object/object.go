3477ccc1d5c0d1dc448d549ceeb25eac00dc44b8
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-03-28 18:03:13 +0900

    Add primitive objects

--- object/object.go
+++ object/object.go
@@ -1,5 +1,7 @@
 package object
 
+import "fmt"
+
 // ObjectType is type string identifier
 type ObjectType string
 
@@ -8,3 +10,34 @@ type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
+)
+
+// Integer is from IntegerLiteral
+type Integer struct {
+	Value int64
+}
+
+// Inspect return "1"
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+// Boolean is from ast.Boolean
+type Boolean struct {
+	Value bool
+}
+
+// Inspect return "true"
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+
+// Null is null
+type Null struct{}
+
+// Inspect return "null"
+func (n *Null) Inspect() string  { return "null" }
+func (n *Null) Type() ObjectType { return NULL_OBJ }
