d5b333db36b85783068e0054e7106d9aa223317d
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-03-28 17:31:19 +0900

    Add comment

--- object/object.go
+++ object/object.go
@@ -1,7 +1,9 @@
 package object
 
+// ObjectType is type string identifier
 type ObjectType string
 
+// Object is all value in program
 type Object interface {
 	Type() ObjectType
 	Inspect() string
