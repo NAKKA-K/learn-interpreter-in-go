80012dea25167c90c3533a71f8f90807f3387a7c
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-03-31 21:55:21 +0900

    Add return object

--- object/object.go
+++ object/object.go
@@ -12,9 +12,10 @@ type Object interface {
 }
 
 const (
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ    = "NULL"
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE_OBJ"
 )
 
 // Integer is from IntegerLiteral
@@ -41,3 +42,12 @@ type Null struct{}
 // Inspect return "null"
 func (n *Null) Inspect() string  { return "null" }
 func (n *Null) Type() ObjectType { return NULL_OBJ }
+
+// ReturnValue is object wrapper
+type ReturnValue struct {
+	Value Object
+}
+
+// Inspect return Inspect() of object
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
