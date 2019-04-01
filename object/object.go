c0348d9ffcb63ab61b24cc48476e6e89ac376df3
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-04-01 15:51:54 +0900

    Add function struct

--- object/object.go
+++ object/object.go
@@ -1,6 +1,12 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/NAKKA-K/learn-interpreter-in-go/ast"
+)
 
 // ObjectType is type string identifier
 type ObjectType string
@@ -17,6 +23,7 @@ const (
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE_OBJ"
 	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
 )
 
 // Integer is from IntegerLiteral
@@ -61,3 +68,30 @@ type Error struct {
 // Inspect return "ERROR: ~"
 func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
+
+// Function is from ast.FunctionLiteral
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
+// Inspect return "fn(...params) { 'BlockStatement.String()' }"
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n}")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
