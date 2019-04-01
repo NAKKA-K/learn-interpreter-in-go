66997f34075eb1279e1f07dae4fbd0a0afd505fd
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-04-02 00:06:25 +0900

    Add outer environment

--- object/environment.go
+++ object/environment.go
@@ -1,5 +1,12 @@
 package object
 
+// NewEnclosedEnvironment is
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
+}
+
 // NewEnvironment return Environment object
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
@@ -9,11 +16,15 @@ func NewEnvironment() *Environment {
 // Environment is map of identifier
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 // Get identifier from environment map
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
