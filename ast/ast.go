a04a036c29bcc028bae80032258700be087c0012
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-04-04 19:13:36 +0900

    Add array literal

--- ast/ast.go
+++ ast/ast.go
@@ -300,3 +300,27 @@ type StringLiteral struct {
 func (sl *StringLiteral) TokenLiteral() string { return sl.Token.Literal }
 func (sl *StringLiteral) String() string       { return sl.Token.Literal }
 func (sl *StringLiteral) expressionNode()      {}
+
+// ArrayLiteral for array
+type ArrayLiteral struct {
+	Token    token.Token // '['
+	Elements []Expression
+}
+
+// TokenLiteral return '['
+func (al *ArrayLiteral) TokenLiteral() string { return al.Token.Literal }
+func (al *ArrayLiteral) String() string {
+	var out bytes.Buffer
+
+	elements := []string{}
+	for _, el := range al.Elements {
+		elements = append(elements, el.String())
+	}
+
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+
+	return out.String()
+}
+func (al *ArrayLiteral) expressionNode() {}
