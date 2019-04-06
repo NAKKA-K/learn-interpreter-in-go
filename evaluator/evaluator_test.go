c3f9773858985576754ff65d4ff836d10f6d9893
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-04-07 03:02:42 +0900

    Add rest of array

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -327,6 +327,8 @@ func TestBuiltinFunction(t *testing.T) {
 		{`last([1, 2, 3])`, 3},
 		{`last([])`, nil},
 		{`last(1)`, "argument to `last` must be ARRAY, got INTEGER"},
+		{`rest([1, 2, 3])`, []int{2, 3}},
+		{`rest([])`, nil},
 	}
 
 	for _, tt := range tests {
