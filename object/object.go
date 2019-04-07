65ead707906ba2a1e0dcfcc54286bf7e7f109b9b
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-04-08 00:52:14 +0900

    Add hashkey type

--- object/object.go
+++ object/object.go
@@ -3,6 +3,7 @@ package object
 import (
 	"bytes"
 	"fmt"
+	"hash/fnv"
 	"strings"
 
 	"github.com/NAKKA-K/learn-interpreter-in-go/ast"
@@ -141,3 +142,35 @@ func (ao *Array) Inspect() string {
 	return out.String()
 }
 func (ao *Array) Type() ObjectType { return ARRAY_OBJ }
+
+// HashKey is from ast.HashLiteral
+type HashKey struct {
+	Type  ObjectType
+	Value uint64
+}
+
+// HashKey return boolean to hash
+func (b *Boolean) HashKey() HashKey {
+	var value uint64
+
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+
+	return HashKey{Type: b.Type(), Value: value}
+}
+
+// HashKey return integer to hash
+func (i *Integer) HashKey() HashKey {
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+
+// HashKey return string to hash
+func (s *String) HashKey() HashKey {
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+
+	return HashKey{Type: s.Type(), Value: h.Sum64()}
+}
