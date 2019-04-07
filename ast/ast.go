b1df852b2b5524ec34dc1a2d6a639bbfe791208d
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-04-07 22:24:15 +0900

    Add hash literal type

--- ast/ast.go
+++ ast/ast.go
@@ -346,3 +346,27 @@ func (ie *IndexExpression) String() string {
 	return out.String()
 }
 func (ie *IndexExpression) expressionNode() {}
+
+// HashLiteral for hash
+type HashLiteral struct {
+	Token token.Token // '{'
+	Pairs map[Expression]Expression
+}
+
+// TokenLiteral return '{'
+func (hl *HashLiteral) TokenLiteral() string { return hl.Token.Literal }
+func (hl *HashLiteral) String() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for key, value := range hl.Pairs {
+		pairs = append(pairs, key.String()+":"+value.String())
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+
+	return out.String()
+}
+func (hl *HashLiteral) expressionNode() {}
